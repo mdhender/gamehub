fb8d2fce710b8f1d1eb22156db6a5463eb235b08
Michael Henderson <mdhender@mdhender.com>
2026-03-30 10:38:58 -0500

    prevent admin seed command from running in production

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,11 +4,18 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
+        if (App::environment('production')) {
+            $this->command->error('AdminSeeder cannot run in production. Use the app:create-admin-user command instead.');
+
+            return;
+        }
+
         User::factory()->admin()->create([
             'name' => 'Penny Guin',
             'email' => 'penguin@gamehub.test',
